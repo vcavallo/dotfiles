763e4f2fff5352959b778258301f00e4098c1fde
vinney cavallo <vcavallo@gmail.com>
2017-11-17 10:33:48 -0500

    Attempt to clean up os x path

--- bash_profile
+++ bash_profile
@@ -1,9 +1,3 @@
 if [ -f ~/.bashrc ]; then
   source ~/.bashrc
 fi
-export PATH="/usr/local/opt/qt@5.5/bin:$PATH"
-
-# Setting PATH for Python 3.5
-# The original version is saved in .bash_profile.pysave
-PATH="/Library/Frameworks/Python.framework/Versions/3.5/bin:${PATH}"
-export PATH
--- darwin_bashrc.sh
+++ darwin_bashrc.sh
@@ -74,11 +74,11 @@
  
     # NODE_PATH
     # Node Path from Homebrew I believe
-    export NODE_PATH="/usr/local/lib/node_modules:$NODE_PATH"
+    # export NODE_PATH="/usr/local/lib/node_modules:$NODE_PATH"
 
-    export VIRTUALENVWRAPPER_PYTHON=/Library/Frameworks/Python.framework/Versions/3.5/bin/python3
-    export WORKON_HOME=~/Envs
-    source /usr/local/bin/virtualenvwrapper.sh
+    # export VIRTUALENVWRAPPER_PYTHON=/Library/Frameworks/Python.framework/Versions/3.5/bin/python3
+    # export WORKON_HOME=~/Envs
+    # source /usr/local/bin/virtualenvwrapper.sh
  
     # PYTHON_SHARE
     # Python Shared Path from Homebrew I believe
@@ -105,30 +105,8 @@
  
   # Paths
  
-    # The USR_PATHS variable will just store all relevant /usr paths for easier usage
-    # Each path is seperate via a : and we always use absolute paths.
- 
-    # A bit about the /usr directory
-    # The /usr directory is a convention from linux that creates a common place to put
-    # files and executables that the entire system needs access too. It tries to be user
-    # independent, so whichever user is logged in should have permissions to the /usr directory.
-    # We call that /usr/local. Within /usr/local, there is a bin directory for actually
-    # storing the binaries (programs) that our system would want.
-    # Also, Homebrew adopts this convetion so things installed via Homebrew
-    # get symlinked into /usr/local
-    export USR_PATHS="/usr/games/bin:/usr/local:/usr/local/bin:/usr/local/sbin:/usr/bin"
+    # export USR_PATHS="/usr/games/bin:/usr/local:/usr/local/bin:/usr/local/sbin:/usr/bin"
 
-    # Hint: You can interpolate a variable into a string by using the $VARIABLE notation as below.
- 
-    # We build our final PATH by combining the variables defined above
-    # along with any previous values in the PATH variable.
- 
-    # Our PATH variable is special and very important. Whenever we type a command into our shell,
-    # it will try to find that command within a directory that is defined in our PATH.
-    # Read http://blog.seldomatt.com/blog/2012/10/08/bash-and-the-one-true-path/ for more on that.
-    export PATH="$USR_PATHS:$PYTHON_SHARE:$PATH"
-
-    # set PATH so it includes user's private bin if it exists
     if [ -d "$HOME/bin" ] ; then
      PATH="$HOME/bin:$PATH"
     fi
@@ -142,51 +120,12 @@
   # show all if ambiguous
   bind "set show-all-if-ambiguous on"
 
-  # Functions to CD into folders from anywhere
-  # so you just type "folder-name".
-  # HINT: It uses the built in USER variable to know your OS X username
-
-  # USE: desktop
-  #      desktop subfolder
-
-  function desktop {
-    cd /Users/$USER/Desktop/$@
-  }
-
   function dots {
     cd /Users/$USER/dotfiles/$@
   }
 
   function dev {
     cd /Users/$USER/development/$@
-  }
-
-  function nvalt {
-    cd /Users/$USER/Dropbox/nvALT && vim
-  }
-
-  function wiki {
-    cd /Users/$USER/Dropbox/wiki && vim
-  }
-
-  function db {
-    cd /Users/$USER/Dropbox/$@
-  }
-
-  function bill {
-    cd /Users/$USER/development/projects/bill-blass$@
-  }
-
-  function curious {
-    cd /Users/$USER/development/projects/wondersauce/google$@
-  }
-
-  function blog {
-    cd /Users/$USER/development/code/vcavallo.github.io/$@
-  }
-
-  function blog_posts {
-    cd /Users/$USER/development/code/vcavallo.github.io/source/_posts/$@
   }
 
   # A function to easily grep for a matching process
@@ -202,63 +141,40 @@
   #      extract imatar.tar
   function extract () {
       if [ -f $1 ] ; then
-	  case $1 in
-	      *.tar.bz2)  tar xjf $1      ;;
-	      *.tar.gz)   tar xzf $1      ;;
-	      *.bz2)      bunzip2 $1      ;;
-	      *.rar)      rar x $1        ;;
-	      *.gz)       gunzip $1       ;;
-	      *.tar)      tar xf $1       ;;
-	      *.tbz2)     tar xjf $1      ;;
-	      *.tgz)      tar xzf $1      ;;
-	      *.zip)      unzip $1        ;;
-	      *.Z)        uncompress $1   ;;
-	      *)          echo "'$1' cannot be extracted via extract()" ;;
-	  esac
+    case $1 in
+        *.tar.bz2)  tar xjf $1      ;;
+        *.tar.gz)   tar xzf $1      ;;
+        *.bz2)      bunzip2 $1      ;;
+        *.rar)      rar x $1        ;;
+        *.gz)       gunzip $1       ;;
+        *.tar)      tar xf $1       ;;
+        *.tbz2)     tar xjf $1      ;;
+        *.tgz)      tar xzf $1      ;;
+        *.zip)      unzip $1        ;;
+        *.Z)        uncompress $1   ;;
+        *)          echo "'$1' cannot be extracted via extract()" ;;
+    esac
       else
-	  echo "'$1' is not a valid file"
+    echo "'$1' is not a valid file"
       fi
   }
-   
-  # A function to easily grep for a matching file
-  # USE: lg filename
-  function lg {
-    FIRST=`echo $1 | sed -e 's/^\(.\).*/\1/'`
-    REST=`echo $1 | sed -e 's/^.\(.*\)/\1/'`
-    ls -la | grep "[$FIRST]$REST"
-  }
-   
-  # USE: new-repo repo-name
-  function new-repo () {
-    curl -u 'vcavallo' https://api.github.com/user/repos -d "{\"name\":\"$1\"}"
-  }
- 
+
+
+
 # Aliases
 # =====================
 
-  # Vim
-  alias vim="/Applications/MacVim.app/Contents/MacOS/Vim"
-  # use mac vim's terminal vim (has clipboard support)
- 
   # Rspec
   alias rs="rspec"
  
   # Ruby
   alias rb="ruby"
-	
+  
   # CS function
-	function cs () {
-		cd $1; ls "-lahG"
-  }	
+  function cs () {
+    cd $1; ls "-lahG"
+  }  
  
-  # Touch and Go
-  create() {
-    touch $1
-    subl $1
-  }
- 
-  # FTP to vinneycavallo.com
-  alias vcftp="ftp ftp://vcavallo@vinneycavallo.com"
  
   # LS
    alias l="ls -lahG | cut -f 6- -d ' '"
@@ -269,7 +185,6 @@
  
   # CD
   alias cd..='cd ..'
-  alias cdbox='cd ~/Dropbox/computers'
  
   # Emacs
   alias emacs='emacs-24.3'
@@ -277,9 +192,6 @@
  
   # GREP
   alias grep='grep -n --color=auto'
- 
-  # Mou.app
-  alias mou="open /Applications/Mou.app"
  
   # Restart POW server for this app
   alias restart="touch tmp/restart.txt"
@@ -306,4 +218,5 @@
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 export PATH="$HOME/.rvm/bin:$PATH" # Add RVM to PATH for scripting
+
 echo "using Darwin-specific bashrc"
